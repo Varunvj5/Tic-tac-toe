8fb745ed9ab6111ec007b7bc954b430f8f0d3d95
Varun <joshivarunvj5@gmail.com>
2021-11-03 22:08:09 +0530

    Added more file

---
+++ 1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+void main()
+{
+    int x = 5, y = 6;
+    int r;
+    r = x * y;
+    printf("%d", r);
+    //  return 0;
+}
---
+++ python.py
@@ -0,0 +1,4 @@
+x="hello"
+y=5
+z=90
+print(x,y,z)
